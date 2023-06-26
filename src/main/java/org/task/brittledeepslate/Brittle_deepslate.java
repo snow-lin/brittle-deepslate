431ad681533d7b1cb0b768633b5e1caa413bc7b7
snow-lin <734936633@qq.com>
2023-06-26 13:00:52 +0800

    Initial commit

---
+++ src/main/java/org/task/brittledeepslate/Brittle_deepslate.java
@@ -0,0 +1,14 @@
+package org.task.brittledeepslate;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Brittle_deepslate implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     *
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
